72157322ab1a62af65b3d17296f714f64fb70f29
Anthony Belcastro <belcastt@live.com>
2020-10-25 15:32:25 -0700

    MongoDB, looks to be an easier option,  We didn't learn Sequelize for react, connections?  I just can't picture rightnow, we can change it later if you guys really want sql.  I think these will work, here is just a pull over and refactor from previous manifest stuff.  I named products we are nameed in there and we will need to add the rest of the class if we want to

---
+++ shopside/src/products.js
@@ -0,0 +1,57 @@
+const products = [
+    {
+        _id: '1',
+        name: 'Travis',
+        image: '/Photos/travis.jpg',
+        description:
+            'Travis has the most memory on his computer',
+        motto: 'Beard no Beard, I am here',
+        category: 'Project three Teammate',
+        salary: "1,000,000",
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        _id: '2',
+        name: 'Frank ',
+        image: '/Photos/frank.jpg',
+        description:
+            'Frank has the worst internet for his computer',
+        motto: 'no Beard, I am here',
+        category: 'Project three Teammate',
+        salary: "1,000,000",
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        _id: '3',
+        name: 'Zackariah',
+        image: '/Photos/Zackaria.jpg',
+        description:
+            'Zackariah has the best bicycle hat for his computer',
+        motto: 'Beard no Beard, I am here',
+        category: 'Project three Teammate',
+        salary: "1,000,000",
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 12,
+    },
+    {
+        _id: '',
+        name: 'Anthony',
+        image: '/Photos/anthony.jpg',
+        description:
+            'Anthony has a beanbag chair next to his computer',
+        motto: 'Beard no Beard, I am here',
+        category: 'Project three Teammate',
+        salary: "1,000,000",
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 12,
+    },
+
+]
+
+export default products
