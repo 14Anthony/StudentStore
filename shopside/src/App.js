da348d919e6860e0a8504c625b6aa10d1d868b33
Anthony Belcastro <belcastt@live.com>
2020-10-25 14:30:48 -0700

    Found ReactBootstrap and bootwatch, that Travis was showing yesterday  I imported them, and you can use the bootstrap components by importing them

--- shopside/src/App.js
+++ shopside/src/App.js
@@ -1,10 +1,14 @@
-
+import React from 'react'
+import Header from './components/Header'
+import Footer from './components/Footer'
 
 const App = () => {
   return (
     <div>
+      <Header />
       <h3> Welcome to storeFront
         </h3>
+      <Footer />
     </div>
   );
 }
--- shopside/src/components/Footer.js
+++ shopside/src/components/Footer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const Footer = () => {
     return (
         <footer>
-
+            ATZF -- footer component
         </footer>
     )
 }
