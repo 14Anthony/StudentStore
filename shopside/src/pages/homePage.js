35d3d65351ac99f7c07ede72e362129e124c7b19
Anthony Belcastro <belcastt@live.com>
2020-10-25 16:39:34 -0700

    Cards render on the hompage with adhoc....I htink I need to make components for each of those. renders, but I did template litterals, and then i destructured  products.js to capture products, in place of using props.

--- shopside/src/pages/homePage.js
+++ shopside/src/pages/homePage.js
@@ -1,26 +1,25 @@
 import React from 'react'
 import { Row, Col } from 'react-bootstrap'
+import Product from '../components/Product'
 import products from '../products'
 
 const HomePage = () => {
     return (
-        <div>
+        <>
             <h1>The most recent Bootcamp Graduates</h1>
             <Row>
-                {products.map(products => (
+                {products.map(product => (
                     // this needs to be a mobile first application so maybe we should just keep it consistent throughout, I am putting this here for copy and paste needs we will have the small devices take up 12 column, medium devices we can take up 6 , large screens take up four columns and lets have extra large screens take up 3 
                     <Col sm={12} md={6} lg={4} xl={3}>
                         {/* we will need somekind of component that renders just the photos as we loop thru the products.js */}
-                        <h3>
-                            {products.name}
-                        </h3>
+                        <Product product={product} />
                     </Col>
 
                 ))}
 
             </Row>
 
-        </div>
+        </>
     )
 }
 
