04d3de7aba8f63973bb3ee4bb33d79bdf017f25a
Zack Shone <zackariahshone@gmail.com>
2020-10-25 15:54:22 -0700

    pushing to new branch

--- shopside/src/App.js
+++ shopside/src/App.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Container } from 'react-bootstrap'
 import Header from './components/Header'
 import Footer from './components/Footer'
-
+import Login from './components/login';
 const App = () => {
   return (
     <div>
@@ -13,6 +13,7 @@ const App = () => {
             Welcome to storeFront
         </h3>
         </Container>
+        <Login />
       </main>
       <Footer />
     </div>
---
+++ shopside/src/components/login.js
@@ -0,0 +1,29 @@
+import React from "react";
+// import Content from "../components/Content";
+
+function Login() {
+  return (
+    <div>
+    <div className = 'container'>
+    </div>
+     <div className = 'container'>
+      <div className = 'row'>
+        <div className = 'col'>
+          <div className = "card">
+          <p>User Name</p>
+          <input></input>
+          <p> Password</p>
+          <input></input>
+          </div>
+        </div>
+        <div className = 'col'>
+          <a href= "#"> Click here if you need to sign up</a>
+        </div>
+      </div>
+
+     </div>
+    </div>
+  );
+}
+
+export default Login;
