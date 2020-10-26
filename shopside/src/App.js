99c065fb49bbf8bde03da1963c2597377df8e992
Anthony Belcastro <belcastt@live.com>
2020-10-25 18:05:03 -0700

    adding empty un used routes for people to connect their paths to

--- shopside/src/App.js
+++ shopside/src/App.js
@@ -1,11 +1,12 @@
 import React from 'react'
+// make sure to check to see if you imported each of the specific components you need.
 import { Container } from 'react-bootstrap'
 import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomePage from './pages/homePage'
 
-// I deleted the HomePage tag, and imported route, given the path = "tosomepath"  it will render component={pulling in the component}  then make sure you have exact path. if you are
+// I deleted the HomePage tag, and imported route, given the path = "tosomepath"  it will render component={pulling in somecomponent}  then make sure you have "exact" path. if you are
 const App = () => {
   return (
     <Router>
@@ -16,6 +17,26 @@ const App = () => {
           <Route
             path='/'
             component={HomePage}
+            exact />
+
+          <Route
+            path='/'
+            component={}
+            exact />
+
+          <Route
+            path='/'
+            component={}
+            exact />
+
+          <Route
+            path='/'
+            component={}
+            exact />
+
+          <Route
+            path='/'
+            component={}
             exact />
         </Container>
       </main>
