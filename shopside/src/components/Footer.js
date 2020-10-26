abbbf4c1acec9b3895c1153d3c0ad7b87d14a672
Anthony Belcastro <belcastt@live.com>
2020-10-25 17:38:59 -0700

    downloaded the react router,....

--- shopside/src/components/Footer.js
+++ shopside/src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Container, Col, Row } from 'react-bootstrap'
-
+// so if you import es7  you can create arrow functions for export immediately, by typingin racfe   
 const Footer = () => {
     return (
         <footer>
--- shopside/src/components/Header.js
+++ shopside/src/components/Header.js
@@ -1,15 +1,24 @@
 import React from 'react'
 import { Container, Nav, Navbar } from 'react-bootstrap'
 
+// so if you import es7  you can create arrow functions for export immediately, by typingin racfe 
+
 const Header = () => {
     return (
         <header>
+
+            {/* copied this off of the git hub repo and deleted drop down menus, but we can add them back if you want. */}
+
+
             <Navbar bg="dark" variant='dark' expand="lg" collapseOnSelect>
                 <Container>
                     <Navbar.Brand href="/">storeFront</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
+
+                            {/* fontawesome for the cart, and users */}
+
                             <Nav.Link href="/cart"><i className='fas fa-shopping-cart'></i>CART</Nav.Link>
 
                             <Nav.Link href="/login"><i className='fas fa-user'></i>LOGIN</Nav.Link>
