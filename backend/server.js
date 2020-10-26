863e0a98757e9110599df85d3977d9c94759b505
Anthony Belcastro <belcastt@live.com>
2020-10-25 21:57:04 -0700

    moved the DBjson, to the back end ran it on 8800 , and the found that you can run a proxy for the local host connecting the back end to the front end prior to the db., allowin me to copy and paste the products manifest to and connect the back and front end together.    Async await from my net ninja call, seems to work on react.  axios documentation.

--- backend/server.js
+++ backend/server.js
@@ -10,5 +10,9 @@ app.get('/', (req, res, next) => {
 app.get('/api/products', (req, res, next) => {
     res.json(products)
 })
+app.get('/api/products/:id', (req, res, next) => {
+    const product = products.find(p => p._id === req.params.id)
+    res.json(product)
+})
 
 app.listen(8800)
