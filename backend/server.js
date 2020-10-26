35983e29853916c1321ec9662a7d146990fef0ce
Anthony Belcastro <belcastt@live.com>
2020-10-25 21:26:24 -0700

    creating the server, changing up the exports from react to node, by editing the module from export

--- backend/server.js
+++ backend/server.js
@@ -1,1 +1,14 @@
 const express = require('express');
+const products = require('./DB/products')
+
+const app = express()
+
+app.get('/', (req, res, next) => {
+    res.send('Backend Begins...')
+})
+
+app.get('/', (req, res, next) => {
+
+})
+
+app.listen(8800)
