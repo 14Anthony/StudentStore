be4db59f1cc8f9f0d3cd739a65747a5d88c6271b
Anthony Belcastro <belcastt@live.com>
2020-10-25 17:49:46 -0700

    creating our first route pattern to follow.  it works, so lets try to keep the code the same way thru.  I deleted the HomePage tag, and imported route, given the path = tosomepath  it will render component={pulling in the component}  then make sure you have exact path. if you are making an adjustment

--- shopside/src/App.js
+++ shopside/src/App.js
@@ -1,20 +1,25 @@
 import React from 'react'
 import { Container } from 'react-bootstrap'
+import { BrowserRouter as Router, Route } from 'react-router-dom'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import HomePage from './pages/homePage'
 
 const App = () => {
   return (
-    <div>
+    <Router>
+
       <Header />
       <main className='py-3' >
         <Container>
-          <HomePage />
+          <Route
+            path='/'
+            component={HomePage}
+            exact />
         </Container>
       </main>
       <Footer />
-    </div>
+    </Router>
   );
 }
 
