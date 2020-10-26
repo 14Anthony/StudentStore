bf1d578986e7aceec5715c28aeb225de4ff889e9
Anthony Belcastro <belcastt@live.com>
2020-10-25 17:50:27 -0700

    psuedo code for jik

--- shopside/src/App.js
+++ shopside/src/App.js
@@ -5,6 +5,7 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomePage from './pages/homePage'
 
+// I deleted the HomePage tag, and imported route, given the path = "tosomepath"  it will render component={pulling in the component}  then make sure you have exact path. if you are
 const App = () => {
   return (
     <Router>
