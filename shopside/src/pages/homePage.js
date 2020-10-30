82c45e2c9ff6aeeed7d884f6131ca839d1c8b027
Anthony Belcastro <belcastt@live.com>
2020-10-30 02:52:23 -0700

    created a store for the devetools, dev tools now show up, created a reducer, which moved to a constant that goes to the actions and down to the homepage.

--- shopside/src/pages/homePage.js
+++ shopside/src/pages/homePage.js
@@ -1,52 +1,44 @@
-import React, { useState, useEffect } from 'react'
-// make sure to check to see if you imported each of the specific components you need.
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-// import products from '../products'moving the products db to the backend. trying to get it to connect, possible an async await with axios.
 import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
 
 const HomePage = () => {
+    const dispatch = useDispatch()
 
-    //getter and setters.
-    const [products, setProducts] = useState([])
-    // axios request....useEffect, async await
+    const productList = useSelector(state => state.productList)
+    const { loading, error, products } = productList
+
+
     useEffect(() => {
-        const getProducts = async () => {
-            const { data } = await axios.get('/api/products')
+        dispatch(listProducts())
 
-            setProducts(data)
-        }
-        getProducts()
-    }, [])
+
+    }, [dispatch])
 
     return (
         <>
+            <h1>
+                Most Recent BootCamp Graduates
+        </h1>
 
-
-            <h1>The most recent Bootcamp Graduates</h1>
-            <Row>
-
-                {products.map(product => (
-
-                    // this needs to be a mobile first application so maybe we should just keep it consistent throughout, I am putting this here for copy and paste needs we will have the small devices take up 12 column, medium devices we can take up 6 , large screens take up four columns and lets have extra large screens take up 3 
-
-                    <Col sm={12} md={6} lg={4} xl={3}>
-
-                        {/* we will need somekind of component that renders just the photos as we loop thru the products.js */}
-
-                        <Product product={product} />
-
-                    </Col>
-
-                ))}
-
-            </Row>
-
-
-
+            {loading
+                ? (<h2>Loading ...</h2>)
+                : error
+                    ? (<h3>{error}</h3>)
+                    : (<Row>
+                        {products.map((product) => {
+                            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                                <Product product={product} />
+                            </Col>
+                        })}
+                    </Row>)}
 
         </>
+
     )
 }
 
