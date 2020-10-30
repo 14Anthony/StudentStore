1966aa6cf063b8fdb75ae8809f82966cab7d0dc3
Anthony Belcastro <belcastt@live.com>
2020-10-29 17:52:49 -0700

    I am using the react styling in the model, to use the models, like  a back end commponents here in the seeder.js file.

--- backend/seeder.js
+++ backend/seeder.js
@@ -1,7 +1,29 @@
 import mongoose from 'mongoose'
 import dotenv from 'dotevn'
 import users from './DB/users.js'
-import users from './DB/products.js'
-import users from './models/userModels.js'
-import users from './models/productModel.js'
-import users from './models/orderModel.js'
+import products from './DB/products.js'
+import Users from './models/userModels.js'
+import Products from './models/productModel.js'
+import Order from './models/orderModel.js'
+
+import connectDB from './config/db.js'
+import Order from './models/orderModel.js'
+import User from './models/userModels.js'
+import Product from './models/productModel.js'
+
+dotenv.config()
+
+connectDB()
+
+const importData = async () => {
+    try {
+        await Order.deleteMany()
+        await Product.deleteMany()
+        await User.deleteMany()
+
+        const createUser = await User.insertMany(users)
+
+    } catch (error) {
+
+    }
+}
