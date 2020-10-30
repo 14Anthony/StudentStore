cf7b63e7866817d0b5f17af48ce9b5cd30a7f607
Anthony Belcastro <belcastt@live.com>
2020-10-30 03:00:29 -0700

    date is data...found the typo

--- shopside/src/actions/productActions.js
+++ shopside/src/actions/productActions.js
@@ -18,11 +18,10 @@ export const listProducts = () => async (dispatch) => {
     } catch (error) {
         dispatch({
             type: PRODUCT_LIST_FAIL,
-            payload: error.response && error.response.date.message
+            payload: error.response && error.response.data.message
                 ? error.response.data.message
-                : error.message
+                : error.message,
         })
 
     }
-
 }
