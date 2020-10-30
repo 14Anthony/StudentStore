b94235dda6db6c43b4a7e5a748be04f781fe2ce4
Anthony Belcastro <belcastt@live.com>
2020-10-29 23:33:39 -0700

    setting up a routes folder on the backend.....I need to be able set it up for POSTMAN captures.

---
+++ backend/routes/productRoutes.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import asyncHandler from 'express-async-handler'
+import Product from '../models/productModel.js'
+
+const router = express.Router()
+// the is going to grab all of the students
+// this will be the GET to /api/products
+// this is access for a public routes
+
+
+router.get('/', asyncHandler(async (req, res, next) => {
+    const products = await Product.find({})
+
+    res.json(products)
+}))
+// the is going to grab single student
+//  GET to /api/products
+// this is access for a public routes
+
+
+
+router.get('/:id', (req, res, next) => {
+    const product = await Product.findById(req.params.id)
+
+    if (product) {
+
+        res.json(product)
+    } else {
+        res.status(404).json({ message: 'product not fount' })
+    }
+})
+
+export default router
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,9 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import connectDB from './config/db.js'
-import products from './DB/products.js'
+// import products from './DB/products.js'
+
+import productRoutes from './routes/productRoutes,js'
 
 
 dotenv.config()
@@ -14,13 +16,8 @@ app.get('/', (req, res, next) => {
     res.send('Backend Begins...')
 })
 
-app.get('/api/products', (req, res, next) => {
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res, next) => {
-    const product = products.find(p => p._id === req.params.id)
-    res.json(product)
-})
+app.use('/api/products', productRoutes)
+
 
 const PORT = process.env.PORT || 8800
 
