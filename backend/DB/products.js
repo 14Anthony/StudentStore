072d5c34fbb5091905ac24a6d6dea18a96fe11f9
Anthony Belcastro <belcastt@live.com>
2020-10-29 23:13:17 -0700

    seed file up and running db. connection

--- backend/DB/products.js
+++ backend/DB/products.js
@@ -55,7 +55,62 @@ const products = [
         rating: 4.5,
         numReviews: 12,
     },
+    {
 
+        name: 'Kevin',
+        image: '/Photos/travis.jpg',
+        brand: 'Newbies',
+        description:
+            'Travis has the most memory on his computer',
+        motto: 'has a mean mustache superman',
+        category: 'Project ond and two Teammate',
+        salary: 100000,
+        countInStock: 1,
+        rating: 1.5,
+        numReviews: 12,
+    },
+    {
+
+        name: 'Kerishma',
+        image: '/Photos/frank.jpg',
+        brand: 'Newbies',
+        description:
+            'Frank has the worst internet for his computer',
+        motto: 'night time coder',
+        category: 'Project two Teammate',
+        salary: 100000,
+        countInStock: 1,
+        rating: 2.5,
+        numReviews: 12,
+    },
+    {
+
+        name: 'Daniel',
+        image: '/Photos/zackariah.jpg',
+        brand: 'Newbies',
+        description:
+            'Zackariah has the best bicycle hat for his computer',
+        motto: 'Beard no Beard, I am here',
+        category: 'Project guitar Teammate',
+        salary: 100000,
+        countInStock: 1,
+        rating: 3.5,
+        numReviews: 12,
+    },
+    {
+
+        name: 'Blake',
+        image: '/Photos/anthony.jpg',
+        brand: 'Newbies',
+        description:
+            'Anthony has a beanbag chair next to his computer',
+        motto: 'Beard no Beard, I am here',
+        category: 'Its all your fault dude',
+        salary: 100000,
+        countInStock: 1,
+        rating: 4.5,
+        numReviews: 12,
+    },
 ]
 
 export default products
