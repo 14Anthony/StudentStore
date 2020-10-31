9983088ea42fb69915df62798a116b83bbc83187
Anthony Belcastro <belcastt@live.com>
2020-10-31 13:32:06 -0700

    ANTHONY'S PORTION COMPLETE

--- backend/server.js
+++ backend/server.js
@@ -1,32 +1,28 @@
 
-import express from 'express'
-import dotenv from 'dotenv'
-import connectDB from './config/db.js'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
 // import products from './DB/products.js'
-import { notFound, errorHandler } from './middleware/errorMid.js'
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMid.js";
 
-import productRoutes from './routes/productRoutes.js'
+dotenv.config();
 
+connectDB();
 
-dotenv.config()
+const app = express();
 
-// connectDB()
-
-const app = express()
-
-
-
-app.get('/', (req, res, next) => {
-    res.send('Backend Begins...')
-})
-
-app.use('/api/products', productRoutes)
-
-app.use(notFound)
-
-app.use(errorHandler)
+app.get("/", (req, res, next) => {
+    res.send("Backend Begins...");
+});
 
 
-const PORT = process.env.PORT || 8800
+app.use("/api/products", productRoutes);
 
-app.listen(8800)
+app.use(notFound);
+
+app.use(errorHandler);
+
+
+const PORT = process.env.PORT || 8800;
+app.listen(8800);
