c5e7175134aa01c71ddbe749c81c9d03b56b0707
bigchsta62 <65703232+bigchsta62@users.noreply.github.com>
2020-10-31 12:43:11 -0700

    fixed server connection

--- backend/server.js
+++ backend/server.js
@@ -1,27 +1,25 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import products from './DB/products.js'
+import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+// import products from './DB/products.js'
 
+import productRoutes from "./routes/productRoutes.js";
+import { notFound, errorHandler } from "./middleware/errorMid.js";
+dotenv.config();
 
-dotenv.config()
+connectDB();
 
-// connectDB()
+const app = express();
 
-const app = express()
+app.get("/", (req, res, next) => {
+  res.send("Backend Begins...");
+});
 
+app.use("/api/products", productRoutes);
 
+app.use(notFound);
+app.use(errorHandler);
 
-app.get('/', (req, res, next) => {
-    res.send('Backend Begins...')
-})
+const PORT = process.env.PORT || 8800;
 
-app.use('/api/products', productRoutes)
-
-app.use()
-
-app.use()
-
-
-const PORT = process.env.PORT || 8800
-
-app.listen(8800)
+app.listen(8800);
